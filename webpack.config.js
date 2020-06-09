38b063c41c3550746f115907f10e1cb662800817
dogpaw <foodev@naver.com>
2020-06-09 19:41:33 +0900

    webpack, 설정파일 사용

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
     entry: './source/index.js',
+    output:{
+      path:path.resolve(__dirname,"public")
+      ,filename:'index_bundle.js'
+    },
     devServer: {
         host : '127.0.0.1',
         contentBase: path.join(__dirname, "/"),
